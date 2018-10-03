4b2cc1bd9c4b44bbc0023139f91ab869e4470e00
LEA <lea.cotan@gmail.com>
2018-10-03 16:56:40 +0300

    Documentation and changed drop menu to entry and buttons

--- GUI.py
+++ GUI.py
@@ -6,11 +6,12 @@ import os
 import sys #.path import append as append_system_path
 import webbrowser
 
+#global variables
+DEFULT_ENTRY_CSV_MSG = "Enter .csv file"
 Fasta_File_name = ""
 csv_file_name = ""
 restriction_file_name = ""
 ouput_file_location = ""
-
 # 3rd option not shown.
 is_3_shown = False
 protein_fasta_filename = ""
@@ -18,24 +19,34 @@ fasta_file_name = ""
 codon_usage_tables_filnames_list = []
 
 def run_Taico():
+    """
+    runs the TaiCo optimization program
+    :return:
+    """
     from TaiCO_windows import TaiCO
     TaiCO.run_taico()
     return
 
-def problem():
-    print("var does not get assigned a value")
-
 def open_hebrew_u_website():
+    """
+    prints the official iGEM website for Hebrew U
+    :return:
+    """
     webbrowser.open("http://2018.igem.org/Team:HebrewU")
 def open_DTU_website():
+    """
+    prints the official iGEM website for DTU
+    :return:
+    """
     webbrowser.open("http://2016.igem.org/Team:DTU-Denmark")
 
 def create_main_window():
+    """
+    creates and runs the main loop for the tool selection screen.
+    :return:
+    """
     #create window
-
     root = Tk()
-
-    v = tk.IntVar()
     root.configure(background='grey')
     root.geometry("700x422")
     root.resizable(width=False, height=False)
@@ -66,15 +77,16 @@ def create_main_window():
 
 
     def run_option():
+        """
+        the function choosing which program to activate
+        :return:
+        """
         root.destroy()
-
         if var.get() == 1:
             create_Hebrew_U_window()
         elif var.get() == 2:
             run_Taico()
 
-        else:
-            problem()
 
     #next button
     Screen1_next = tk.Button(root,text="Next",font=("Calibri", 15) , command = run_option )
@@ -91,7 +103,7 @@ def create_main_window():
     Buttonimage.place(x=0,y=370)
 
     gui_description = Label(root, text="Multi-functional optimization tool",font=("Calibri", 12),bg="#464444",fg="white")
-
+    #placing
     gui_description.place(x=238,y=40)
     gui_Mul.place(x=283,y=4)
     gui_T.place(x=335,y=4)
@@ -105,8 +117,11 @@ def create_main_window():
 
 #-------------------------------HebrewU 1 window------------------------------------------
 def create_Hebrew_U_window():
+    """
+    creates and runs the main loop for the first screen of the MOOLTI tool.
+    :return:
+    """
     root2 = Tk()
-    v = tk.IntVar()
     root2.configure(background='#00B2B2')
     root2.geometry("700x422")
     root2.resizable(width=False, height=False)
@@ -122,16 +137,24 @@ def create_Hebrew_U_window():
     #gui name
     gui_MulT = Label(root2, text="MulT",font=("Calibri", 23),bg="#00B2B2",fg="white")
     gui_descriptio_MulT = Label(root2, text="Multiple organism optimization by tRNA freq",font=("Calibri", 12),bg="#00B2B2",fg="white")
-
+    #create labels
     fasta_label = Label(root2, text="Upload protein fasta file :", bg="#F0F0F0",font=("calibri",11))
     restriction_label = Label(root2, text="Upload restriction sites file (optional) :", bg="#F0F0F0",font=("calibri",11))
     destination_label = Label(root2, text="Enter destination Folder:", bg="#F0F0F0",font=("calibri",11))
 
     def get_back_to_main1():
+        """
+        destroys the current screen and returns to previous screen.
+        :return:
+        """
         root2.destroy()
         create_main_window()
 
     def second_menu():
+        """
+        validates input, continues to second menu
+        :return:
+        """
         global restriction_file_name
         global ouput_file_location
         global protein_fasta_filename
@@ -160,65 +183,66 @@ def create_Hebrew_U_window():
     Screen1_next2 = tk.Button(root2,text="Next",font=("Calibri", 15 ), command =second_menu )
     #back_button
     screen1_back = tk.Button(root2,text="Back",font=("Calibri", 15) , command = get_back_to_main1 )
-
-
-    file2 = Label(root2, bg="red")
-    file3 = Label(root2, bg="red")
-
+    #creates entrys
     output_entry = Entry(root2)
     fasta_entry = Entry(root2)
     restriction_entry = Entry(root2)
+
     global ouput_file_location
     global protein_fasta_filename
     global restriction_file_name
-
+    #makes sure the value isn't lost between screens
     fasta_entry.insert(0, protein_fasta_filename)
     restriction_entry.insert(0, restriction_file_name)
     output_entry.insert(0, ouput_file_location)
 
     # select file with sequences
     def input_file2():
+        """
+        inserts the input into the fasta variable
+        :return:
+        """
         # open the file with the sequences
         global protein_fasta_filename
         if protein_fasta_filename != "":
             fasta_entry.delete(0, END)
         protein_fasta_filename = filedialog.askopenfilename()
-        # show only real name of file
-        # protein_fasta_filename = protein_fasta_filename.split('/')[-1]
         fasta_entry.insert(0, protein_fasta_filename)
 
-    # check or not box to produce final result -> main analysis part
     def input_file3():
         # open the file with the sequences
         global restriction_file_name
         if restriction_file_name != "":
             restriction_entry.delete(0, END)
+        messagebox.showinfo('MOOTI 2018',
+                            'Please enter a text file containing restriction enzymes names, sperated by a comma.'
+                            ' for example:\n EcoRI,HindIII,BamHI \n It\'s case sensitive !')
+
         restriction_file_name = filedialog.askopenfilename()
-        # show only real name of file
-        # restriction_file_name = restriction_file_name.split('/')[-1]
         with open(restriction_file_name, 'r') as content_file:
              content = content_file.read()
         restriction_entry.insert(0, content)
 
     def output_file():
+        """
+        saves the ouput file location
+        :return:
+        """
         # open the file with the sequences
         global ouput_file_location
         if ouput_file_location != "":
             output_entry.delete(0, END)
         ouput_file_location = filedialog.askdirectory()
-
-        # ouput_file_location = ouput_file_location.split('/')[-1]
+        #show in entry
         output_entry.insert(0, ouput_file_location)
 
+    #create buttons
     fasta_search_button = Button(root2, text="Search file", fg="black", bg="white", command=input_file2, relief=RAISED,
                      borderwidth=3)
     search_restriction_button = Button(root2, text="Search file", fg="black", bg="white", command=input_file3, relief=RAISED,
                      borderwidth=3)
     output_search_button = Button(root2, text="Select folder", fg="black", bg="white", command=output_file, relief=RAISED,
                      borderwidth=3)
-
-
-
 
     # place buttons in window by coordinates
     fasta_search_button.place(x=380, y=163)
@@ -238,7 +262,6 @@ def create_Hebrew_U_window():
 
     ouput_file_location = output_entry.get()
 
-
     gui_MulT.place(x=320,y=4)
     gui_descriptio_MulT.place(x=210,y=40)
     root2.mainloop()
@@ -246,8 +269,11 @@ def create_Hebrew_U_window():
 
 #-------------------------------HebrewU 2 window------------------------------------------
 def create_second_hebrew_U_window():
+    """
+    creates and runs the main loop for the first screen of the MOOLTI tool.
+    :return:
+    """
     root3 = Tk()
-    v = tk.IntVar()
     root3.configure(background='#00B2B2')
     root3.geometry("700x422")
     root3.resizable(width=False, height=False)
@@ -255,6 +281,10 @@ def create_second_hebrew_U_window():
 
     global codon_usage_tables_filnames_list
     def get_back_to_second_window():
+        """
+        closes the current screen and proceedes to the second menu
+        :return:
+        """
         root3.destroy()
         create_Hebrew_U_window()
 
@@ -269,94 +299,106 @@ def create_second_hebrew_U_window():
     gui_MulT4 = Label(root3, text="MulT", font=("Calibri", 23), bg="#00B2B2", fg="white")
     gui_descriptio_MulT4 = Label(root3, text="Multiple organism optimization by tRNA freq",font=("Calibri", 12),bg="#00B2B2",fg="white")
 
+    # select file with sequences
+    def input_file(v):
+        """
+        a generalized input function
+        :param v: the entry variable to fill
+        :return:
+        """
+        # open the file with the sequences
+        v.insert(0, filedialog.askopenfilename())
 
     # Buttons and labels for more and optimizze buttons
     def addmore():
-
+        """
+        shows one or two more buttons
+        :return:
+        """
         global is_3_shown
         if is_3_shown == False:
             label_3.place(x=100, y=244)
-            v3.place(x=130, y=244)
+            v3.place(x=430, y=244)
+            third_organism_button.place(x=340, y=244)
             is_3_shown = True
 
         else:
             label_4.place(x=100, y=296)
-            v4.place(x=130, y=296)
+            v4.place(x=430, y=296)
+            fourth_organism_button.place(x=340, y=296)
+
 
     #Buttons and labels for more and optimizze buttons
     button_more = Button(root3, text="Add more organisms", bg="#F0F0F0", font=("calibri", 15), command=addmore)
     button_back = Button(root3, text="Back", bg="#F0F0F0", font=("calibri", 15), command=get_back_to_second_window)
-    label_compatible = Label(root3, text="Compatible organisms (by priority):", bg="#F0F0F0", font=("calibri", 15))
-    label_1 = Label(root3, text="1.", bg="#F0F0F0", font=("calibri", 15))
-    label_2 = Label(root3, text="2.", bg="#F0F0F0", font=("calibri", 15))
-    label_3 = Label(root3, text="3.", bg="#F0F0F0", font=("calibri", 15))
-    label_4 = Label(root3, text="4.", bg="#F0F0F0", font=("calibri", 15))
+    first_organism_button = Button(root3, text="Search file", fg="black", bg="white", command=lambda: input_file(v1), relief=RAISED,
+                                 borderwidth=3)
+    second_organism_button = Button(root3, text="Search file", fg="black", bg="white", command=lambda: input_file(v2),
+                                       relief=RAISED,
+                                       borderwidth=3)
+    third_organism_button = Button(root3, text="Search file", fg="black", bg="white", command=lambda: input_file(v3),
+                                  relief=RAISED,
+                                  borderwidth=3)
+    fourth_organism_button = Button(root3, text="Search file", fg="black", bg="white", command=lambda: input_file(v4),
+                                   relief=RAISED,
+                                   borderwidth=3)
+
+    #create labels
+    label_compatible = Label(root3, text="Compatible organisms :", bg="#F0F0F0", font=("calibri", 15))
+    label_1 = Label(root3, text="Add organism codon usage table file:", bg="#F0F0F0", font=("calibri", 11))
+    label_2 = Label(root3, text="Add organism codon usage table file:", bg="#F0F0F0", font=("calibri", 11))
+    label_3 = Label(root3, text="Add organism codon usage table file:", bg="#F0F0F0", font=("calibri", 11))
+    label_4 = Label(root3, text="Add organism codon usage table file:", bg="#F0F0F0", font=("calibri", 11))
+    #placements
     button_back.place(x = 100, y = 350)
     button_more.place(x=230, y=350)
+    first_organism_button.place(x=340, y=140)
+    second_organism_button.place(x=340, y=192)
     label_compatible.place(x=100, y=100)
     label_1.place(x=100, y=140)
     label_2.place(x=100, y=192)
 
-    # list1
-    first_organizm_name = StringVar(root3)
-    first_organizm_name.set("First pick")  # default value
-    v1 = OptionMenu(root3, first_organizm_name, "none", "Arabidopsis thaliana", "Saccharomyces cerevisiae", "homosapiens",
-                    "Escherichia coli")
-    v1.place(x=130, y=140)
+    #first organizm
+    v1 = Entry(root3)
+    v1.insert(0,DEFULT_ENTRY_CSV_MSG)  # default value
+    v1.place(x=430, y=140)
+    #second organizm
+    v2 = Entry(root3)
+    v2.insert(0, DEFULT_ENTRY_CSV_MSG)  # default value
+    v2.place(x=430, y=192)
 
-    # list2
-    second_organizm_name = StringVar(root3)
-    second_organizm_name.set("Second pick")  # default value
-    v2 = OptionMenu(root3, second_organizm_name, "Arabidopsis thaliana", "Saccharomyces cerevisiae", "homosapiens",
-                    "Escherichia coli")
-    v2.place(x=130, y=192)
+    # 3rd organizm
+    v3 = Entry(root3)
+    v3.insert(0, DEFULT_ENTRY_CSV_MSG)
+    # 4th organizm
+    v4 = Entry(root3)
+    v4.insert(0, DEFULT_ENTRY_CSV_MSG)
 
-    # list3
-
-    Third_organizm_name = StringVar(root3)
-    Third_organizm_name.set("Third pick")  # default value
-    v3 = OptionMenu(root3, Third_organizm_name, "none", "Arabidopsis thaliana", "Saccharomyces cerevisiae", "homosapiens",
-                    "Escherichia coli")
-
-    # list4
-    fourth_organizm_name = StringVar(root3)
-    fourth_organizm_name.set("Fourth pick")  # default value
-    v4 = OptionMenu(root3, fourth_organizm_name, "none", "Arabidopsis thaliana", "Saccharomyces cerevisiae", "homosapiens",
-                    "Escherichia coli")
-
-    switch_dict = {"Caenorhabditis elegans" : "CodonOptimization\organism_files\c_elegans_6239.csv",
-                   "Bacillus subtilis" : r"CodonOptimization\organism_files\b_subtilis_1423.csv",
-                   "homosapiens" : "CodonOptimization\organism_files\h_sapiens_9606.csv",
-                   "Escherichia coli" : "CodonOptimization\organism_files\e_coli_316407.csv",
-                   "Arabidopsis thaliana" : ""}
-
-
-    suceeded_printing = BooleanVar()
     boolvar = IntVar()
     boolvar.set(3)
 
     def run_optimization():
+        """
+        uses user input to run the MOOLTI Codon Optimization
+        :return:
+        """
         sys.path.append(os.path.join(sys.path[0], "CodonOptimization"))
         import Main
         global protein_fasta_filename
-        global codon_usage_tables_filnames_list
-        name1 = first_organizm_name.get()
-        name2 = second_organizm_name.get()
-        name3 = Third_organizm_name.get()
-        name4 = fourth_organizm_name.get()
-        print(name3)
-        print(name4)
-        global codon_usage_tables_filnames_list
-        if name1 != "First pick":
-            codon_usage_tables_filnames_list.append(switch_dict[name1])
-            print("added")
-        if name2 != "Second pick":
-            codon_usage_tables_filnames_list.append(switch_dict[name2])
-        # if name3 != "none" and name3 != "Third pick":
-        #     codon_usage_tables_filnames_list.append(switch_dict[name3])
-        # if name4 != "none" and name4 != "Fourth pick":
-        #     codon_usage_tables_filnames_list.append(switch_dict[name4])
-        # codon_usage_tables_filnames_list = []
+        name1 =  v1.get()
+        name2 =  v1.get()
+        name3 =  v1.get()
+        name4 =  v1.get()
+        codon_usage_tables_filnames_list = []
+        if name1 != DEFULT_ENTRY_CSV_MSG:
+            codon_usage_tables_filnames_list.append(name1)
+
+        if name2 != DEFULT_ENTRY_CSV_MSG:
+            codon_usage_tables_filnames_list.append(name2)
+        if name3 != DEFULT_ENTRY_CSV_MSG:
+            codon_usage_tables_filnames_list.append(name3)
+        if name4 != DEFULT_ENTRY_CSV_MSG:
+            codon_usage_tables_filnames_list.append(name4)
         protein_fasta_filename = protein_fasta_filename
         global ouput_file_location
 
@@ -365,30 +407,27 @@ def create_second_hebrew_U_window():
                          output_destination=ouput_file_location)
 
     def handle_click():
-        print("boolvar is " + str(boolvar.get()))
+        """
+        handles the button click, and the sucsess or failure of the Codon optimization function
+        :return:
+        """
 
         success = run_optimization()
-        #root3.after(0, run_optimization)
         if success == 1:
-            print("yay")
             messagebox.showinfo('MulT 2018', "Optimization successful. Output printed to specified location")
             root3.destroy()
             create_main_window()
         elif success == 2:
             messagebox.showinfo('MulT 2018', "Optimization failed.")
-        #print("boolvar is " + str(boolvar.get()))
 
     Screen3_Optimize = Button(root3, text="Optimize", font=("Calibri", 15), command=handle_click)
-
-
     Screen3_Optimize.place(x=500, y=350)
+
 
     gui_MulT4.place(x=320,y=4)
     gui_descriptio_MulT4.place(x=210,y=40)
 
     root3.mainloop()
-
-
 
 if __name__ == '__main__':
 
